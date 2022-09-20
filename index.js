f02dbb7aecee41af38bf31e17af3db15dc7a964d
Harrison <harriwilson5534@gmail.com>
2022-09-20 12:49:00 -0400

    commit tree 7d73af2dc5bd8e5f025c186016fd367876e154db

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Write your solution in this file!
+const employee = {
+    name: "John Favreau",
+    streetAddress: "123 Main St.",
+}
+//console.log(employee)
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const updateEmployee = {...obj};
+    updateEmployee[key] = value;
+    return updateEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const employeeClone = {...obj};
+    delete employeeClone[key];
+    return employeeClone; 
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//const employee = {
+//    name : "Jimmy McGill",
+  //  streetAddress : "123 Main St",
+//}
+
+//function updateEmployeeWithKeyAndValue(obj, key, value) {
+//    const employeeUpdate = {...employee};
+//    employeeUpdate.streetAddress = value;
+//    return employeeUpdate;
+//}
+
+//function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+//    employee.streetAddress = value;
+//    return employee;
+//}
+
+//function deleteFromEmployeeByKey(obj, key) {
+//    const employeeUpdate = {...employee};
+//    delete employeeUpdate.name;
+//    return employeeUpdate;
+//}
+
+//function destructivelyDeleteFromEmployeeByKey(obj, key) {
+//    delete employee.name;
+//    return employee;
+//}
